218cfee964bad654a6eef3e33890ec4dabda5d1c
“hugoescalpelo” <hugoescalpelo@gmail.com>
2021-07-28 18:45:04 -0500

    Carga de Estructura báscia
    Unicamente el archivo de base, correctamente comentado

---
+++ Estructura_Basica/Estructura_Basica.ino
@@ -0,0 +1,29 @@
+/*
+ * Estructura básica
+ * Por: Hugo Vargas
+ * Fecha: 28 de junio de 2021
+ * 
+ * Esto es una muestra de la estructura básica de un programa
+ */
+
+// Bibliotecas
+
+// Constantes
+
+// Variables
+
+// Definición de objetos
+
+// Condiciones iniciales - Se ejecuta sólo una vez al energizar
+void setup() {// Inicio de void setup ()
+  // Aquí va tu código
+
+}// Fin de void setup
+
+// Cuerpo del programa - Se ejecuta constamente
+void loop() {// Inicio de void loop
+  // put your main code here, to run repeatedly:
+
+}// Fin de void loop
+
+// Funcioes del usuario
